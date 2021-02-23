6b8d9e5f45f717b07096cbb53c5a083617c90d3a
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-02-23 16:58:14 -0300

    Implementing routes using HTTP verbs and methods.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
 import express from 'express';
 const app = express();
 
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello, World! - NLW04' });
+});
+
+app.post('/', (request, response) => {
+    return response.json({ message: 'Data successfully saved' });
+});
+
 app.listen(3333, () => console.log('\n', 'SERVER RUNNING ON PORT 3333'));
