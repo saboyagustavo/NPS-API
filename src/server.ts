891ce34377be1dc20f0509569e54aa52d056d277
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-02-23 22:41:48 -0300

    Implemented a router and a controller.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import './database';
+import { router } from './router';
+
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello, World! - NLW04' });
-});
-
-app.post('/', (request, response) => {
-    return response.json({ message: 'Data successfully saved' });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('\n', 'SERVER RUNNING ON PORT 3333'));
