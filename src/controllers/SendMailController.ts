48babc6ae2a94a1482e061aafc02c91e542e7006
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-02-26 22:38:35 -0300

    Validating if user has been already registered.

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -29,20 +29,31 @@ class SendMailController {
             });
         }
 
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description,
+            user_id: user.id,
+            link: process.env.URL_MAIL,
+        };
+
+        const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
+
+        const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+            where: [{ user_id: user.id }, { value: null }],
+        });
+
+        if (surveyUserAlreadyExists) {
+            await SendMailService.execute(email, survey.title, variables, npsPath);
+            return response.json({ surveyUserAlreadyExists });
+        }
+
         const surveyUser = surveysUsersRepository.create({
             user_id: user.id,
             survey_id,
         });
 
         await surveysUsersRepository.save(surveyUser);
-
-        const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs');
-
-        const variables = {
-            name: user.name,
-            title: survey.title,
-            description: survey.description,
-        };
 
         await SendMailService.execute(email, survey.title, variables, npsPath);
         return response.status(201).json(surveyUser);
